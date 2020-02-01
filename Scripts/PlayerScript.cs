1cf95ccfbd0e3ad883ecd1941e07937a58a38c5c
Mistogun <aaron.gomez.de.segura@gmail.com>
2020-02-01 00:42:11 -0600

    First commit

---
+++ Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+	public float speed;
+	public float maxSpeed;
+	private Vector3 position;
+
+
+    void Update()
+    {
+		Vector3 aux = this.transform.position;
+        if(Input.GetKey(KeyCode.A))
+		{
+			this.transform.position = new Vector3(aux.x - speed * Time.deltaTime, aux.y, 0f);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			this.transform.position = new Vector3(aux.x + speed * Time.deltaTime, aux.y, 0f);
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			this.transform.position = new Vector3(aux.x, aux.y + speed * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			this.transform.position = new Vector3(aux.x, aux.y - speed * Time.deltaTime, 0f);
+		}
+	}
+}
