559f5c386ab486e4efe86b67f8944e119052eee1
Mistogun <aaron.gomez.de.segura@gmail.com>
2020-02-01 09:58:44 -0600

    Better player move

--- Scripts/PlayerScript.cs
+++ Scripts/PlayerScript.cs
@@ -7,6 +7,8 @@ public class PlayerScript : MonoBehaviour
 	public float speed = 5f;
 	public float maxSpeed;  //Sin usar 
 
+	public float angle;
+
 	private int[] objetosColeccionados = new int[3];	//guarda los objetos que se han coleccionado
 	//hacer una variable para el objeto que tenga actualmente
 
@@ -25,12 +27,16 @@ public class PlayerScript : MonoBehaviour
 	void Movement()
 	{
 		if (Input.GetKey(KeyCode.A))
-			this.transform.Translate(new Vector3(-speed * Time.deltaTime, 0f, 0f));
+			this.transform.Translate(new Vector3(-speed * Time.deltaTime,(+speed * Time.deltaTime * 0.55f), 0f));
+		//this.transform.Translate(new Vector3(-speed * Time.deltaTime, +speed * Time.deltaTime + (+speed * Time.deltaTime * 0.33f), 0f));
 		if (Input.GetKey(KeyCode.D))
-			this.transform.Translate(new Vector3(+speed * Time.deltaTime, 0f, 0f));
+			this.transform.Translate(new Vector3(+speed * Time.deltaTime,(-speed * Time.deltaTime * 0.55f), 0f));
+		//this.transform.Translate(new Vector3(+speed * Time.deltaTime, -speed * Time.deltaTime + (+speed * Time.deltaTime * 0.33f), 0f));
 		if (Input.GetKey(KeyCode.W))
-			this.transform.Translate(new Vector3(0f, +speed * Time.deltaTime, 0f));
+			this.transform.Translate(new Vector3(+speed * Time.deltaTime * angle, +speed * Time.deltaTime, 0f));
+		//this.transform.Translate(new Vector3(+speed * Time.deltaTime + (+speed * Time.deltaTime*0.33f), +speed * Time.deltaTime, 0f));
 		if (Input.GetKey(KeyCode.S))
-			this.transform.Translate(new Vector3(0f, -speed * Time.deltaTime, 0f));
+			this.transform.Translate(new Vector3(-speed * Time.deltaTime * angle, -speed * Time.deltaTime, 0f));
+		//this.transform.Translate(new Vector3(-speed * Time.deltaTime + (+speed * Time.deltaTime * 0.33f), -speed * Time.deltaTime, 0f));
 	}
 }
