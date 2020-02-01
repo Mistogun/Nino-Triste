6092c97c447f98bb9a814a687e2eea7a15d37358
raratchet <maxmew_19@hotmail.com>
2020-02-01 09:02:18 -0600

    Audio and Camara

---
+++ Scripts/CamaraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraControl : MonoBehaviour
+{
+    public Camera previous;
+    public Camera next;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag == "Player")
+        {
+            previous.gameObject.SetActive(false);
+            next.gameObject.SetActive(true);
+        }
+    }
+}
